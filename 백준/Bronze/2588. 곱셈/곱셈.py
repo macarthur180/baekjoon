fcf946d38498bd228b44c9dc71e87892ac10cb6a
macarthur180 <77783448+macarthur180@users.noreply.github.com>
2024-01-22 21:10:36 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+a= int(input())
+b = input()
+
+line3 = a * int(b[2:3])
+line4 = a * int(b[1:2])
+line5 = a * int(b[0:1])
+
+sum = a * int(b)
+
+
+print(line3)
+print(line4)
+print(line5)
+
+print(sum)
